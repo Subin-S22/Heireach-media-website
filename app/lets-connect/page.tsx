1f1a9879374eee2d79ba248ff96e795f526c20a4
subin.s22@outlook.com <subin@DESKTOP-M1LRVFD.>
2025-02-24 10:37:23 +0000

    chore: heading

--- app/lets-connect/page.tsx
+++ app/lets-connect/page.tsx
@@ -1,3 +1,9 @@
 export default function LetsConnect() {
-  return <div>LetsConnect</div>
+  return (
+    <div>
+      <h2>Hei, React out us!</h2>
+      <p>Ready to elevate your Brand to new heights </p>
+      <p>Let&apos;s make it happen.</p>
+    </div>
+  );
 }
