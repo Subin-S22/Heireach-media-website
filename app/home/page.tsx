47d4f22b02c6427125b56ce82ae52b0992c58647
vishnu-lilly <vishnu.mowdgalayabm@network.lilly.com>
2025-02-24 18:47:51 +0530

    added missing import

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,4 @@
-// import CompanyDescription from "@/features/home/CompanyDescription";
+import CompanyDescription from "@/features/home/CompanyDescription";
 import InitialView from "@/features/home/InitialView";
 import OurServices from "@/features/home/OurServices";
 import Image from "next/image";
