b2175113ca337441105b05045292fb8b563a9478
subin.s22@outlook.com <subin@DESKTOP-M1LRVFD.>
2025-02-24 17:25:28 +0000

    fix: build error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,6 @@
 import CompanyDescription from "@/features/home/CompanyDescription";
 import InitialView from "@/features/home/InitialView";
 import OurServices from "@/features/home/OurServices";
-import Image from "next/image";
 import OurWorks from "./components/our-works";
 import WhyChooseUs from "../about-us/components/why-choosen-us";
 
@@ -10,7 +9,6 @@ export default function Home() {
     <div className="">
       <main className="">
         <InitialView />
-        {/* <Space className="w-full h-4" /> */}
         <CompanyDescription />
         <OurServices />
         <WhyChooseUs />
