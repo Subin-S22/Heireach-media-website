09cc9ae3152d3d3f5740856ba5963a7e7c1aa097
subin.s22@outlook.com <subin@DESKTOP-M1LRVFD.>
2025-03-03 10:55:02 +0000

    feat: mobile navbar in sidebar

--- lib/ui/navbar/index.tsx
+++ lib/ui/navbar/index.tsx
@@ -4,6 +4,7 @@ import style from "./index.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 
 const navbarContent = [
   {
@@ -27,11 +28,109 @@ const navbarContent = [
     href: "/lets-connect",
   },
 ];
+
+const serviceContent = [
+  { name: "Digital Marketing", href: "/services/digital" },
+  { name: "Web & App Development", href: "/services/webapp" },
+  { name: "Marker Place & Marketing", href: "/services/webapp" },
+  { name: "E-Commerce & Marketing", href: "/services/e-commerce" },
+  { name: "Production", href: "/services/production" },
+  { name: "Print Media & Public Relations (PR)", href: "/services/media" },
+];
+
 export default function Navbar() {
   const [mouseEnter, setMouseEnter] = useState<boolean>(false);
+  const [openSidebar, setOpenSidebar] = useState<boolean>(false);
+  const [showSerivces, setShowServices] = useState<boolean>(false);
+
   return (
-    <div className="flex justify-between items-center px-8 py-4 sticky top-0 bg-[#0a0a0a] z-50 max-w-screen-xl mx-auto">
+    <div
+      className="flex justify-between items-center px-2 lg:px-8 py-4 sticky 
+    top-0 bg-[#0a0a0a] z-50 max-w-screen-xl mx-auto"
+    >
       <div className={classnames(style.flex, style.gap_4)}>
+        <AnimatePresence>
+          {openSidebar ? (
+            <div className="relative">
+              {/**backdrop */}
+              <div
+                className="absolute z-[5] backdrop-blur-md w-[calc(100vw+2rem)] h-screen -top-8 -left-8"
+                onClick={() => setOpenSidebar(false)}
+              ></div>
+              {/**sidebar */}
+              <motion.aside
+                initial={{ x: -100 }}
+                animate={{ x: 0 }}
+                exit={{ x: -100 }}
+                transition={{ duration: 0.2, ease: "easeInOut" }}
+                className={`absolute  -top-2 -left-2 z-10 bg-black w-[60vw] md:w-[50vw] max-w-md h-screen `}
+              >
+                <ul>
+                  {navbarContent.map((content) => {
+                    return content.name !== "Services" ? (
+                      <li
+                        key={content.name}
+                        className="text-sm md:text-base font-amazingSlab px-4 py-2"
+                        onClick={() =>
+                          // setTimeout(() => setOpenSidebar(false), 200)
+                          setOpenSidebar(false)
+                        }
+                      >
+                        <Link href={content.href}>{content.name}</Link>
+                      </li>
+                    ) : (
+                      <li
+                        key={content.name}
+                        className="text-sm md:text-base font-amazingSlab px-4 py-2"
+                        onClick={() => setShowServices(!showSerivces)}
+                      >
+                        <div className="flex gap-2">
+                          {content.name}
+                          <svg
+                            className="-mr-1 size-5 text-gray-400"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                            data-slot="icon"
+                          >
+                            <path d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" />
+                          </svg>
+                        </div>
+                        {showSerivces && (
+                          <ul className="px-4 list-outside">
+                            {serviceContent.map((value) => (
+                              <Link
+                                key={value.name}
+                                href={value.href}
+                                className="p-4 block"
+                                onClick={() => setOpenSidebar(false)}
+                              >
+                                {value.name}
+                              </Link>
+                            ))}
+                          </ul>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </motion.aside>
+            </div>
+          ) : null}
+        </AnimatePresence>
+        {/**bread */}
+        <div
+          className="block lg:hidden text-white text-2xl font-bold p-1 rounded-lg max-h-max rotate-90"
+          onClick={() => setOpenSidebar(!openSidebar)}
+        >
+          {/* <Image
+            src="/breadcrumbs.svg"
+            width={30}
+            height={30}
+            alt="breadcrumbs"
+          /> */}
+          |||
+        </div>
         <Image src="/images/logo.svg" alt="logo" width={40} height={40} />
         <div className="leading-0">
           <div className="font-amazingSlab font-extrabold text-xl md:text-3xl text-white whitespace-nowrap">
@@ -45,7 +144,7 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-      <ul className={"hidden md:flex justify-between items-center gap-8"}>
+      <ul className={"hidden lg:flex justify-between items-center gap-8"}>
         {navbarContent.map((content) => {
           return (
             <li
@@ -109,24 +208,11 @@ const DropDown = ({ mouseEnter, setMouseEnter }: DropDownProps) => {
           aria-labelledby="menu-button"
         >
           <div className="py-1">
-            <Link href="/services/digital" className={dropClass}>
-              Digital Marketing
-            </Link>
-            <Link href="/services/webapp" className={dropClass}>
-              Web & App Development
-            </Link>
-            <Link href="/services/webapp" className={dropClass}>
-              Market Place & Marketing
-            </Link>
-            <Link href="/services/e-commerce" className={dropClass}>
-              E-Commerce & Marketing
-            </Link>
-            <Link href="/services/production" className={dropClass}>
-              Production
-            </Link>
-            <Link href="/services/media" className={dropClass}>
-              Print Media & Public Relations (PR)
-            </Link>
+            {serviceContent.map((value) => (
+              <Link href={value.href} className={dropClass} key={value.name}>
+                {value.name}
+              </Link>
+            ))}
           </div>
         </div>
       ) : null}
