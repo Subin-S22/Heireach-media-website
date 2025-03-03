4fa5bb51e8b1f72b8b1c2224d9586ad1d1b10de3
subin.s22@outlook.com <subin@DESKTOP-M1LRVFD.>
2025-03-03 15:06:14 +0000

    fix: font family

--- features/home/CompanyDescription.tsx
+++ features/home/CompanyDescription.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import style from "./index.module.css";
 import classnames from "@/lib/function/classnames";
 import CustomSection from "@/lib/ui/customSection";
 
@@ -26,7 +25,7 @@ export default function CompanyDescription() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <div className="relative font-amazingSlab">
+    <div className="relative">
       <CustomSection sectionId="company-description-side-section">
         <motion.div
           initial={{ opacity: 0 }}
@@ -35,41 +34,37 @@ export default function CompanyDescription() {
           // className="flex justify-center items-center min-h-[600px] sm:min-h-[700px] md:min-h-[800px] px-4 sm:px-6 lg:px-7"
           className="p-10 pb-20 pt-36"
         >
-          <div
-            id="company-description"
-            className="w-full"
-          >
-            <div className="flex items-center justify-start gap-4">
-              <div className={classnames(style.h1)}>At
-              <span               className={classnames(
-                  style.h1,
-                  "text-[var(--bg-blue-100)] w-full whitespace-pre-wrap ps-4"
-                )}
-              >
-                HEIREACH MEDIA
-              </span>
+          <div id="company-description" className="w-full">
+            <div className="flex items-center justify-start gap-4 ">
+              <div className="font-amazingSlab font-extrabold text-4xl py-2 z-10">
+                At
+                <span
+                  className={
+                    "text-[var(--bg-blue-100)] w-full whitespace-pre-wrap px-8"
+                  }
+                >
+                  HEIREACH MEDIA
+                </span>
               </div>
             </div>
 
             <div
               className={classnames(
-                style.h2,
-                "flex text-[var(--bg-blue-100)] text-2xl"
+                "flex text-[var(--bg-blue-100)] text-xl font-amazingSlab font-bold"
               )}
             >
               WE DON&apos;T JUST MARKET BRANDS - WE BRING THEIR STORIES TO LIFE
             </div>
 
             <div
-              className={classnames(
-                style.h5,
-                "text-[var(--bg-grey-100)] text-base"
-              )}
+              className={
+                "text-[var(--bg-grey-100)] text-base font-amazingSlab py-4"
+              }
             >
               Our mission is simple yet powerful
             </div>
 
-            <div className={classnames(style.h4, "text-white text-base")}>
+            <div className={"text-white text-base font-amazingSlab py-2"}>
               To craft innovative, customized solutions that align with your
               goals and position your brand at the forefront of the market. With
               a team of creative visionaries, state-of-the-art tools, and an
@@ -79,15 +74,15 @@ export default function CompanyDescription() {
             <div className="w-full h-[auto]">
               <div
                 className={classnames(
-                  style.h2,
-                  "text-[var(--bg-blue-100)] text-base mb-2"
+                  "text-[var(--bg-blue-100)] text-xl font-bold mb-2 font-amazingSlab pt-2 pb-4"
                 )}
               >
                 We ensure your brand not only stands out but shines.
               </div>
             </div>
-
-            <div className={classnames(style.h4, "text-white text-base")}>
+            <div
+              className={classnames("text-white text-base font-amazingSlab")}
+            >
               We believe every brand has a unique story to tell, & our job is to
               amplify it in a way that captivates and inspires. By combining
               creativity with strategy, we turn challenges into opportunities
