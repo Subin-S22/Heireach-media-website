d64374337ab7e74df4c7b7211205a7bd3c6ee3b2
subin.s22@outlook.com <subin@DESKTOP-M1LRVFD.>
2025-03-03 05:05:12 +0000

    chore: type for infocard approach section

--- features/home/OurApproach.tsx
+++ features/home/OurApproach.tsx
@@ -7,7 +7,13 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
-const InfoCard = ({ src, heading, content }) => {
+interface InfoCardProps {
+  src: string;
+  heading: string;
+  content: string;
+}
+
+const InfoCard = ({ src, heading, content }: InfoCardProps) => {
   return (
     <div className="p-5">
       <div
@@ -31,7 +37,7 @@ const InfoCard = ({ src, heading, content }) => {
         <div
           className={classnames(
             style.h4,
-            "whitespace-pre-wrap text-center max-w-72",
+            "whitespace-pre-wrap text-center max-w-72"
           )}
         >
           {content}
@@ -91,7 +97,7 @@ export default function OurApproach() {
     <CustomSection sectionId="our-approach-side">
       <motion.div
         initial={{ opacity: 0 }}
-        animate={isVisible ? { opacity: 1 } : { opacity: 0 }}
+        whileInView={{ opacity: 1 }}
         transition={{ duration: 2, ease: "anticipate" }}
       >
         <div
